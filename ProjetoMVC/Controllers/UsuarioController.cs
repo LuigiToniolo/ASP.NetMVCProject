a4920d0bb4e5df2e25005429aa58bb1ac49e446e
LuigiToniolo <luigi.toniolo@domvsit.com.br>
2022-09-27 15:25:10 -0300

    atualizando maquina

--- ProjetoMVC/Controllers/UsuarioController.cs
+++ ProjetoMVC/Controllers/UsuarioController.cs
@@ -1,12 +1,48 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using ProjetoMVC.Models;
+using ProjetoMVC.Repositorio;
+using System;
+using System.Collections.Generic;
 
 namespace ProjetoMVC.Controllers
 {
     public class UsuarioController : Controller
     {
+        private readonly IUsuarioRepositorio _usuarioRepositorio;
+        public UsuarioController(IUsuarioRepositorio usuarioRepositorio)
+        {
+            _usuarioRepositorio = usuarioRepositorio;
+        }
         public IActionResult Index()
         {
+            List<UsuarioModel> usuarios = _usuarioRepositorio.BuscarTodos();
+
+            return View(usuarios);
+        }
+        public IActionResult CriarContato()
+        {
             return View();
+        }
+
+        [HttpPost]
+        public IActionResult Criar(UsuarioModel usuario)
+        {
+            try
+            {
+                if (ModelState.IsValid)
+                {
+                    usuario = _usuarioRepositorio.Adicionar(usuario);
+
+                    TempData["MensagemSucesso"] = "Contato cadastrado com sucesso!";
+                    return RedirectToAction("Index");
+                }
+                return View(usuario);
+            }
+            catch (Exception erro)
+            {
+                TempData["MensagemErro"] = "$Ops, não conseguimos cadastrar seu contato, tente novamente, detalhe do erro: {erro.Message}";
+                return RedirectToAction("Index");
+            }
         }
     }
 }
